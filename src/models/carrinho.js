86ba09c7f7b4b469023137b49b3524ca4d42573d
Agnes <agnes.rosa@hotmail.com>
2022-01-25 14:09:22 -0300

    feat remove itens no carrinho

--- src/models/carrinho.js
+++ src/models/carrinho.js
@@ -1,34 +1,47 @@
 
-const addToCart = document.getElementsByClassName('li__button')
-addToCart.addEventListener('click', CreateCart(products).createEachItem)
+
 
 class CreateCart {
     constructor({products}){
         this.products = products
     }
 
-    createEachItem(){
+    createItem(){
+        const addToCart = document.getElementsByClassName('li__button')
+        addToCart.addEventListener('click', function(){
+            for(let index = 0; index < this.products.length; index++){
+                let currentProduct = this.products[index]
 
-        for(let index = 0; index < this.products.length; index++){
-            let currentProduct = this.products[index]
+                const image     = document.createElement('img')
+                const title     = document.createElement('h2')
+                const category  = document.createElement('span')
+                const price     = document.createElement('h3')
+                const remove    = document.createElement('span')
+                const item      = document.createElement('li')
+                const list      = document.getElementById('fullBox--shopKart__list')
 
-            const image     = document.createElement('img')
-            const title     = document.createElement('h2')
-            const category  = document.createElement('span')
-            const price     = document.createElement('h3')
-            const remove    = document.createElement('span')
+                image.setAttribute('class','cart__image')
+                title.setAttribute('class','cart__title')
+                category.setAttribute('class','cart__category')
+                price.setAttribute('class','cart__price')
+                remove.setAttribute('class','cart__remove')
 
-            image.setAttribute('class','cart__image')
-            title.setAttribute('class','cart__title')
-            category.setAttribute('class','cart__category')
-            price.setAttribute('class','cart__price')
-            remove.setAttribute('class','cart__remove')
+                image.src          = currentProduct.imagem
+                title.innerText    = currentProduct.nome
+                category.innerText = currentProduct.categoria
+                price.innerText    = currentProduct.preco
+                remove.innerText   = 'x'
 
-            image.src          = currentProduct.imagem
-            title.innerText    = currentProduct.nome
-            category.innerText = currentProduct.categoria
-            price.innerText    = currentProduct.preco
-            remove.innerText   = 'x'
-        }
+                list.appendChild(item)
+                item.append(image,title,category,price,remove)
+            }
+        })
+    }
+
+    removeItem() {
+        const remove = document.getElementsByClassName('cart__remove')
+        remove.addEventListener('click', function(){
+            remove.parentElement.innerHTML = ''
+        })
     }
 }
