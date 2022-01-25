1d58333b421aadf956c95e653e3c96950c052de0
Cayo Rodrigues <cayo.rodrigues1914@gmail.com>
2022-01-25 20:00:57 -0300

    feat: cria metodo para atualizar produto proprio (extra)

--- src/routes/ProductsRouter.js
+++ src/routes/ProductsRouter.js
@@ -2,6 +2,7 @@ class ProductsRouter {
     static baseUrl  = 'https://kenzie-food-api.herokuapp.com'
     static token    = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTgsImlhdCI6MTY0MzExNzgwMywiZXhwIjoxNjQzOTgxODAzLCJzdWIiOiJbb2JqZWN0IFVuZGVmaW5lZF0ifQ.W6gaI05R-6x2sIR09GWfR9L2juHP2KktvKrDgSq_Bt8'
 
+    // GET PRODUTO/PRODUTOS (PADRÃO)
     static
     async get(id='') {
         return await fetch(`${this.baseUrl}/product/${id}`)
@@ -10,6 +11,7 @@ class ProductsRouter {
         .catch(error => error)
     }
 
+    // GET PRODUTO/PRODUTOS QUE VOCÊ ADICIONOU (EXTRA)
     static
     async getOwnProducts(id='') {
         return await fetch(`${this.baseUrl}/my/product/${id}`, {
@@ -20,6 +22,7 @@ class ProductsRouter {
         .catch(error => error)
     }
 
+    // POST NOVO PRODUTO (EXTRA)
     static
     async postOwnProduct(data) {
         return await fetch(`${this.baseUrl}/my/product`, {
@@ -32,6 +35,22 @@ class ProductsRouter {
         })
         .then(res => res.json())
         .then(newProduct => newProduct)
+        .catch(error => error)
+    }
+
+    // ATUALIZAR PRODUTO QUE VOCÊ ADICIONOU (EXTRA)
+    static
+    async updateOwnProduct(id, data) {
+        return await fetch(`${this.baseUrl}/my/product/${id}`, {
+            headers: {
+                'Authorization': `Token ${this.token}`,
+                'Content-Type': 'application/json'
+            },
+            method: 'patch',
+            body: JSON.stringify(data)
+        })
+        .then(res => res.json())
+        .then(updatedProduct => updatedProduct)
         .catch(error => error)
     }
 }
