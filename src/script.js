544156fcc6503fc0ae77030108da2ec56bf8a6b5
Cayo Rodrigues <cayo.rodrigues1914@gmail.com>
2022-01-25 13:48:19 -0300

    test: funcionalidade de listar produtos na vitrine
    Está listando corretamente. Precisa ajustar apenas os estilos.
    Tive que corrigir alguns detalhezinhos na classe para poder funcionar, e também adicionar uma tag <ul> dentro do container da vitrine.

--- src/script.js
+++ src/script.js
@@ -1,8 +1,12 @@
+import { CreateLayout } from "./models/vitrine.js"
 import { ProductsRouter } from "./routes/ProductsRouter.js"
 
 // roda assim que a página carrega
 async function starter() {
     const allProducts = await ProductsRouter.get()
     localStorage.setItem('allProducts', JSON.stringify(allProducts))
+
+    const createLayout = new CreateLayout(allProducts)
+    createLayout.createEachProduct()
 }
 starter()
