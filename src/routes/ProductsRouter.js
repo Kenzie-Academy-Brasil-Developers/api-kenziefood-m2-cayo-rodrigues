d8f3734210070f0e1f49bf017675e52084818ea7
Cayo Rodrigues <cayo.rodrigues1914@gmail.com>
2022-01-25 19:32:24 -0300

    feat: cria metodo para pegar produtos proprios (extra)

--- src/routes/ProductsRouter.js
+++ src/routes/ProductsRouter.js
@@ -1,9 +1,20 @@
 class ProductsRouter {
-    static baseUrl = 'https://kenzie-food-api.herokuapp.com'
+    static baseUrl  = 'https://kenzie-food-api.herokuapp.com'
+    static token    = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTgsImlhdCI6MTY0MzExNzgwMywiZXhwIjoxNjQzOTgxODAzLCJzdWIiOiJbb2JqZWN0IFVuZGVmaW5lZF0ifQ.W6gaI05R-6x2sIR09GWfR9L2juHP2KktvKrDgSq_Bt8'
 
     static
     async get(id='') {
         return await fetch(`${this.baseUrl}/product/${id}`)
+        .then(res => res.json())
+        .then(products => products)
+        .catch(error => error)
+    }
+
+    static
+    async getOwnProducts(id='') {
+        return await fetch(`${this.baseUrl}/my/product/${id}`, {
+            headers: { Authorization: `Token ${this.token}` }
+        })
         .then(res => res.json())
         .then(products => products)
         .catch(error => error)
