b59be70e4ffd35d3937e38394ac9e964977c83ca
occasum13 <daybreak98@hotmail.com>
2022-01-26 01:05:20 -0300

    feat: full layout + filter/search

--- src/models/vitrine.js
+++ src/models/vitrine.js
@@ -2,9 +2,9 @@ class CreateLayout {
     constructor(products){
         this.products = products
     }
-
-    createEachProduct() {
-
+                                                                        //fiz pequenas mudanças no código para organizar o layout
+    createEachProduct() {                                               //como os spans para p, a ordem dos itens lá no itemBox.append
+                                                                        //e a correção de um ou outro typo
         for(let index = 0; index < this.products.length; index++){
             let currentProduct = this.products[index]
 
@@ -13,8 +13,8 @@ class CreateLayout {
             const name            = document.createElement('h2')
             const image           = document.createElement('img')
             const description     = document.createElement('p')
-            const category        = document.createElement('span')
-            const price           = document.createElement('span')
+            const category        = document.createElement('p')
+            const price           = document.createElement('p')
             const addToCart       = document.createElement('button')
             const list            = document.querySelector('ul')
 
@@ -22,9 +22,9 @@ class CreateLayout {
             name.innerText        = currentProduct.nome
             image.src             = currentProduct.imagem
             description.innerText = currentProduct.descricao
-            category.innerText    = currentProduct.categoria
-            price.innerText       = currentProduct.preco
-            addToCart.innerText   = 'Add'
+            category.innerText    = currentProduct.categoria /* icone categorias */
+            price.innerText       = currentProduct.preco /* sifrão e virgulas */
+            addToCart.innerText   = ''/*colocar icone carrinho*/
 
             //setta atributos
             itemBox.setAttribute('class','li')
@@ -32,13 +32,13 @@ class CreateLayout {
             name.setAttribute('class','li__name')
             image.setAttribute('class','li__image')
             description.setAttribute('class','li__description')
-            category.setAttribute('class','li_category')
+            category.setAttribute('class','li__category')
             price.setAttribute('class','li__price')
             addToCart.setAttribute('class','li__button')
 
             //setta pais e filhos
             list.appendChild(itemBox)
-            itemBox.append(name,image,description,category,price,addToCart)
+            itemBox.append(image,category,name,description,price,addToCart)
         }
     }
 }
