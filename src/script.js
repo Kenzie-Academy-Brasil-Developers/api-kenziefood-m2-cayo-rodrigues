e663c20146024f90ba7208df309b43ebebe32749
Cayo Rodrigues <cayo.rodrigues1914@gmail.com>
2022-01-27 08:08:23 -0300

    refactor: junta funções de filtro por categoria em uma só

--- src/script.js
+++ src/script.js
@@ -21,6 +21,9 @@ async function starter() {
     // identificar clicks nos products dentro do carrinho
     const cartContainer = document.querySelector('#shopKart')
     cartContainer.onclick = handleClickOnCart
+
+    const filterButtons = document.querySelectorAll('.filters__button')
+    filterButtons.forEach(button => button.onclick = handleClickOnFilters)
 }
 starter()
 
@@ -49,53 +52,20 @@ function handleClickOnCart(event) {
     }
 }
 
-// funcionalidades de botões de filtro/pesquisa ***** favor conferir rotas ******
-
-const todosButton = document.getElementById('filters__button--todos');
-todosButton.addEventListener('click', filterTodos);
-
-function filterTodos () {
+function handleClickOnFilters(event) {
+    const button        = event.currentTarget
+    const category      = button.dataset.category
     const allProducts   = JSON.parse(localStorage.getItem('allProducts'))
-    const createLayout = new CreateLayout(allProducts)
+    let filteredList    = allProducts
+
+    if (category !== 'Todos') {
+        filteredList = allProducts.filter(product => {
+            return product.categoria === category
+        })
+    }
+    const createLayout = new CreateLayout(filteredList)
     createLayout.createEachProduct()
 }
-
-const panificadoraButton = document.getElementById('filters__button--panificadora');
-panificadoraButton.addEventListener('click', filterPanificadora);
-
-function filterPanificadora () {
-    const allProducts   = JSON.parse(localStorage.getItem('allProducts'))
-    const listPanificadora = allProducts.filter((product) => {
-        return product.categoria === 'Panificadora';
-    });
-    const createLayout = new CreateLayout(listPanificadora)
-    createLayout.createEachProduct()
-}
-
-const frutasButton = document.getElementById('filters__button--frutas');
-frutasButton.addEventListener('click', filterFrutas);
-
-function filterFrutas () {
-    const allProducts   = JSON.parse(localStorage.getItem('allProducts'))
-    const listFrutas = allProducts.filter((product) => {
-        return product.categoria === 'Frutas';
-    });
-    const createLayout = new CreateLayout(listFrutas)
-    createLayout.createEachProduct()
-}
-
-const bebidasButton = document.getElementById('filters__button--bebidas')
-bebidasButton.addEventListener('click', filterBebidas);
-
-function filterBebidas () {
-    const allProducts   = JSON.parse(localStorage.getItem('allProducts'))
-    const listBebidas = allProducts.filter((product) => {
-        return product.categoria === 'Bebidas';
-    });
-    const createLayout = new CreateLayout(listBebidas)
-    createLayout.createEachProduct()
-}
-
 
 const entrada = document.getElementById ('search')
 entrada.addEventListener('keyup', filterSearch);
